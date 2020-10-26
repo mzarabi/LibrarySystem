319886180e5ceb1809fe617688704f052234304b
marcuszarabi <zarabi.marcus@hotmail.com>
2020-10-26 10:36:12 +0100

    added print

--- src/Testing.java
+++ src/Testing.java
@@ -5,6 +5,7 @@ public class Testing {
 		// TODO Auto-generated method stub
 System.out.println("Hello, World!");
 System.out.println("FUNGERAR DETTA ELLERRRRRRRRRRRRRRRRRRRRRRRRRRR!");
+System.out.println("HELLO");
 	}
 
 }
