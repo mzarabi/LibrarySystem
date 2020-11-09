37ecda953254858b49d3708ce5b75a4ca6de58c0
marcuszarabi <zarabi.marcus@hotmail.com>
2020-11-09 17:32:43 +0100

    Create LibraryInterface.java
    Interface for library

---
+++ src/LibraryInterface.java
@@ -0,0 +1,7 @@
+import java.util.ArrayList;
+
+public interface LibraryInterface{
+	ArrayList<Product> products = new ArrayList<Product>();
+	ArrayList<Customer> customers = new ArrayList<Customer>();
+	
+}
