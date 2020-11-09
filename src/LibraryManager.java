d3a85837129219f885cae695351968bc343541a6
marcuszarabi <zarabi.marcus@hotmail.com>
2020-11-09 17:33:46 +0100

    Update LibraryManager.java
    Added help command. Changed add command. Fixed som various exceptions.

--- src/LibraryManager.java
+++ src/LibraryManager.java
@@ -1,39 +1,38 @@
 import java.io.File;
-import java.io.IOException;
 import java.util.Scanner;
 
 enum Command {
-	LIST, CHECKOUT, CHECKIN, REGISTER, DEREGISTER, INFO, QUIT, UNKNOWN, MOVIE, BOOK
+
+	LIST, CHECKOUT, CHECKIN, REGISTER, DEREGISTER, INFO, QUIT, UNKNOWN, HELP
 }
 
 public class LibraryManager {
 
 	ProductLibrary productLibrary = new ProductLibrary();
+	CustomerLibrary customerLibrary = new CustomerLibrary();
+
 	Scanner sc = new Scanner(System.in);
 
-	public static void main(String[] args) throws IOException {
-
-		LibraryManager manager = new LibraryManager();
-		manager.start();
-
-	}
-
-	public void start() throws IOException {
+	public void start() {
 
 		boolean running = true;
 		System.out.println("Welcome!");
 		File bookFile = new File("library.csv");
+		File customerFile = new File("customer.csv");
 
 		if (bookFile.exists()) {
 			productLibrary.readFile();
+			if (customerFile.exists()) {
+				customerLibrary.readCustomerFile();
+			}
 		}
 
-		System.out.println("Succesfully initialized system state from files.");
 		System.out.println("\nCurrent inventory:");
 		System.out.println(productLibrary);
+		System.out.println("Type 'help' to see available commands.\n");
 
 		while (running) {
-			System.out.print("> ");
+			System.out.print("Enter a command > ");
 			String userInput = sc.nextLine();
 			Command command = parseCommand(userInput);
 
@@ -41,13 +40,7 @@ public class LibraryManager {
 
 			switch (command) {
 			case REGISTER:
-				System.out.println("What are you registering? Book (b), Movie (m)");
-				break;
-			case BOOK:
-				addBookCommand();
-				break;
-			case MOVIE:
-				addMovieCommand();
+				addProductCommand();
 				break;
 			case DEREGISTER:
 				removeCommand(arguments);
@@ -59,17 +52,20 @@ public class LibraryManager {
 				displayInventory();
 				break;
 			case CHECKOUT:
-				lendingCommand(arguments);
+				checkOutCommand(arguments);
 				break;
 			case CHECKIN:
-				returnLentCommand(arguments);
+				checkInCommand(arguments);
+				break;
+			case HELP:
+				helpCommand();
 				break;
 			case QUIT:
 				running = false;
 				System.out.println("Good bye!");
 				break;
 			case UNKNOWN:
-				System.out.println("Unknown command, please try again");
+				System.out.println("Unknown command, type register try again\n");
 
 			}
 		}
@@ -77,77 +73,129 @@ public class LibraryManager {
 		sc.close();
 	}
 
-	private void addBookCommand() throws IOException {
+	private void addProductCommand() {
+		System.out.println("What are you registering? Book (b), Movie (m)");
+		String userInput = sc.nextLine();
+		if (userInput.equals("b")) {
+			addBookCommand();
+		} else if (userInput.equals("m")) {
+			addMovieCommand();
+		} else {
+			System.out.println("Unknown command, try again.\n");
+			return;
+		}
+	}
 
-		int bookID = 0;
+	private void addBookCommand() {
+		int bookID;
 		String bookTitle;
 		int bookValue;
 		int pages;
 		String author;
+		String status = "(in stock)";
 
-		System.out.println("\nEnter product ID: ");
-		bookID = sc.nextInt();
-		sc.nextLine();
+		try {
+			System.out.println("\nEnter product ID: ");
+			bookID = Integer.parseInt(sc.nextLine());
 
-		System.out.println("\nEnter title:  ");
-		bookTitle = sc.nextLine();
+			System.out.println("\nEnter title:  ");
+			bookTitle = sc.nextLine();
 
-		System.out.println("\nEnter value:  ");
-		bookValue = sc.nextInt();
+			System.out.println("\nEnter value:  ");
+			bookValue = Integer.parseInt(sc.nextLine());
 
-		System.out.println("\nEnter pages: ");
-		pages = sc.nextInt();
-		sc.nextLine();
+			System.out.println("\nEnter pages: ");
+			pages = Integer.parseInt(sc.nextLine());
+		} catch (NumberFormatException e) {
+			System.out.println("Error! Please enter an Integer, type register to try again.\n");
+			return;
+		}
 		System.out.println("\nEnter author: ");
 		author = sc.nextLine();
 
-		Book newBook = new Book(bookID, bookTitle, bookValue, pages, author);
+		Book newBook = new Book(bookID, bookTitle, bookValue, pages, author, status);
 		productLibrary.addProduct(newBook);
-		
 
 	}
 
-	private void addMovieCommand() throws IOException {
+	private void addMovieCommand() {
 
 		int movieID;
 		String movieTitle;
 		int movieValue;
 		int length;
 		float rating;
+		String status = "(in stock)";
 
-		System.out.println("\nEnter product ID: ");
-		movieID = sc.nextInt();
-		sc.nextLine();
-		System.out.println("\nEnter title:  ");
-		movieTitle = sc.nextLine();
+		try {
+			System.out.println("\nEnter product ID: ");
+			movieID = Integer.parseInt(sc.nextLine());
 
-		System.out.println("\nEnter value:  ");
-		movieValue = sc.nextInt();
+			System.out.println("\nEnter title:  ");
+			movieTitle = sc.nextLine();
 
-		System.out.println("\nEnter length: ");
-		length = sc.nextInt();
-		sc.nextLine();
+			System.out.println("\nEnter value:  ");
+			movieValue = Integer.parseInt(sc.nextLine());
+
+			System.out.println("\nEnter length: ");
+			length = Integer.parseInt(sc.nextLine());
+		} catch (NumberFormatException e) {
+			System.out.println("Error! ID has to be an Integer, type register to try again.\n");
+			return;
+		}
 		System.out.println("\nEnter rating: ");
-		rating = sc.nextFloat();
-		sc.nextLine();
-		
-		Movie newMovie = new Movie(movieID, movieTitle, movieValue, length, rating);
+		try {
+			rating = sc.nextFloat();
+			sc.nextLine();
+		} catch (NumberFormatException e) {
+			System.out.println("Error! Rating has to be an Float, type register to try again.\n");
+			return;
+		}
+
+		Movie newMovie = new Movie(movieID, movieTitle, movieValue, length, rating, status);
 		productLibrary.addProduct(newMovie);
 
 	}
-	
 
-	private void removeCommand(String[] arguments) throws IOException {
+	public void checkOutCommand(String[] arguments) {
 		int ID;
 		try {
 			ID = Integer.parseInt(arguments[0]);
-		} catch (IndexOutOfBoundsException e) {
-			System.out.println("Failed to parse");
+		} catch (Exception e) {
+			System.out.println("Syntax error! Please enter a valid ID number\n");
+			return;
+
+		}
+
+		customerLibrary.checkOutProduct(ID);
+
+	}
+
+	public void checkInCommand(String[] arguments) {
+		int ID;
+		try {
+			ID = Integer.parseInt(arguments[0]);
+		} catch (Exception e) {
+			System.out.println("Syntax error! Please enter a valid ID number\n");
+			return;
+
+		}
+
+		customerLibrary.checkInProduct(ID);
+	}
+
+	private void removeCommand(String[] arguments) {
+		int ID;
+		try {
+			ID = Integer.parseInt(arguments[0]);
+		} catch (Exception e) {
+			System.out.println("Syntax error! Please enter a valid ID number\n");
 			return;
 
 		}
 
 		productLibrary.removeProduct(ID);
+		customerLibrary.removeCustomer(ID);
 
 	}
 
@@ -155,44 +203,32 @@ public class LibraryManager {
 		int ID;
 		try {
 			ID = Integer.parseInt(arguments[0]);
-		} catch (NumberFormatException e) {
-			System.out.println("Please enter a id");
+		} catch (Exception e) {
+			System.out.println("Syntax error! Please enter a valid ID number\n");
 			return;
 
 		}
 		productLibrary.productInfo(ID);
 	}
 
-	private void displayInventory() throws IOException {
+	private void displayInventory() {
 
-		System.out.println(productLibrary);
-
+		if (LibraryInterface.products.isEmpty()) {
+			System.out.println("Library is empty, type register to add a product.\n");
+		} else {
+			System.out.println(productLibrary);
+		}
 	}
 
-	private void lendingCommand(String[] arguments) throws IOException {
-
-		String lendingStatus = "(L)";
-		int ID;
-		try {
-			ID = Integer.parseInt(arguments[0]);
-		} catch (IndexOutOfBoundsException e) {
-			System.out.println("Not a valid ID number");
-			return;
-		}
-
-	}
-
-	private void returnLentCommand(String[] arguments) throws IOException {
-
-		String lendingStatus = "(L)";
-		int ID;
-		try {
-			ID = Integer.parseInt(arguments[0]);
-		} catch (IndexOutOfBoundsException e) {
-			System.out.println("Not a valid ID number");
-			return;
-		}
-
+	public void helpCommand() {
+		System.out.println("Following functionalities are available: \n");
+		System.out.println("REGISTER- Add a product.");
+		System.out.println("DEREGISTER(ID)- Remove a product.");
+		System.out.println("LIST- Show a list of products in the library.");
+		System.out.println("INFO(ID)- See full info about a product.");
+		System.out.println("CHECKOUT(ID)- Check out a product.");
+		System.out.println("CHECKIN(ID)- Return a product.");
+		System.out.println("QUIT- Close the program.\n");
 	}
 
 	private Command parseCommand(String userInput) {
@@ -208,10 +244,12 @@ public class LibraryManager {
 			return Command.DEREGISTER;
 		case "info":
 			return Command.INFO;
-		case "m":
-			return Command.MOVIE;
-		case "b":
-			return Command.BOOK;
+		case "checkout":
+			return Command.CHECKOUT;
+		case "checkin":
+			return Command.CHECKIN;
+		case "help":
+			return Command.HELP;
 		default:
 			return Command.UNKNOWN;
 		}
